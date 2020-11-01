ca7434369200e30064bddc483903e8fa178e0c1f
manikmmalhotra <manikmalhotra2113@gmail.com>
2020-11-01 10:23:09 +0530

    initial

---
+++ app/src/main/java/com/example/android_mvvm/ui/auth/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android_mvvm.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android_mvvm.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
